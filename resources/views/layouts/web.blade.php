a624f664f263100784e5481298996f1c13a2db0f
maxhandx7 <74608008+maxhandx7@users.noreply.github.com>
2024-10-12 12:01:32 -0500

    12-10-2024

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -107,56 +107,6 @@
                         target="_blank">&nbsp;AF</a> </b> <i class="far fa-heart text-danger"></i>&nbsp;</span>
         </div>
     </div>
-    <!-- Portfolio Modals-->
-    <!-- Portfolio Modal 1-->
-    @if (!is_null($proyectos) && $proyectos->isNotEmpty())
-        @foreach ($proyectos as $proyect)
-            <div class="portfolio-modal modal fade" id="portfolioModal{{ $proyect->id }}" tabindex="-1"
-                aria-labelledby="portfolioModal1" aria-hidden="true">
-                <div class="modal-dialog modal-xl">
-                    <div class="modal-content">
-                        <div class="modal-header border-0"><button class="btn-close" type="button"
-                                data-bs-dismiss="modal" aria-label="Close"></button></div>
-                        <div class="modal-body text-center pb-5">
-                            <div class="container">
-                                <div class="row justify-content-center">
-                                    <div class="col-lg-8">
-                                        <!-- Portfolio Modal - Title-->
-                                        <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">
-                                            {{ $proyect->title }}</h2>
-                                        <!-- Icon Divider-->
-                                        <div class="divider-custom">
-                                            <div class="divider-custom-line"></div>
-                                            <div class="divider-custom-icon"><i class="fas fa-code"></i></div>
-                                            <div class="divider-custom-line"></div>
-                                        </div>
-                                        <!-- Portfolio Modal - Image-->
-                                        <img class="img-fluid rounded mb-5" src="{{ $proyect->image }}"
-                                            alt="..." />
-                                        <!-- Portfolio Modal - Text-->
-                                        <h5 class="mb-4">{{ $proyect->description }}</h5>
-
-                                        <p class="">{!! $proyect->long_description !!}</p>
-                                        <a class="btn btn-primary" href="{{ $proyect->link }}">
-                                            <i class="fas fa-arrow-right fa-fw"></i>
-                                            Ver más
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    @endif
-
-
-
-
-
-
-
 
     {!! Html::script('afdeveloper/js/bootstrap.bundle.min.js') !!}
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,8 @@
                 @if (!is_null($proyectos) && $proyectos->isNotEmpty())
                     @foreach ($proyectos as $proyect)
                         <div class="col-md-6 col-lg-4 mb-5">
-                            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                            <div class="portfolio-item mx-auto" data-bs-toggle="modal"
+                                data-bs-target="#portfolioModal{{ $proyect->id }}">
                                 <div
                                     class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                     <div class="portfolio-item-caption-content text-center text-white"><i
@@ -51,6 +52,43 @@
                                 </div>
 
                                 <img class="img-fluid" src="{{ asset($proyect->image) }}" alt="..." />
+                            </div>
+                        </div>
+                        <div class="portfolio-modal modal fade" id="portfolioModal{{ $proyect->id }}" tabindex="-1"
+                            aria-labelledby="portfolioModal1" aria-hidden="true">
+                            <div class="modal-dialog modal-xl">
+                                <div class="modal-content">
+                                    <div class="modal-header border-0"><button class="btn-close" type="button"
+                                            data-bs-dismiss="modal" aria-label="Close"></button></div>
+                                    <div class="modal-body text-center pb-5">
+                                        <div class="container">
+                                            <div class="row justify-content-center">
+                                                <div class="col-lg-8">
+                                                    <!-- Portfolio Modal - Title-->
+                                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">
+                                                        {{ $proyect->title }}</h2>
+                                                    <!-- Icon Divider-->
+                                                    <div class="divider-custom">
+                                                        <div class="divider-custom-line"></div>
+                                                        <div class="divider-custom-icon"><i class="fas fa-code"></i></div>
+                                                        <div class="divider-custom-line"></div>
+                                                    </div>
+                                                    <!-- Portfolio Modal - Image-->
+                                                    <img class="img-fluid rounded mb-5" src="{{ $proyect->image }}"
+                                                        alt="..." />
+                                                    <!-- Portfolio Modal - Text-->
+                                                    <h5 class="mb-4">{{ $proyect->description }}</h5>
+
+                                                    <p class="">{!! $proyect->long_description !!}</p>
+                                                    <a class="btn btn-primary" href="{{ $proyect->link }}">
+                                                        <i class="fas fa-arrow-right fa-fw"></i>
+                                                        Ver más
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     @endforeach
